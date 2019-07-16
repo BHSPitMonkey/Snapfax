058776f8a193d5a84be74e7ce45b83fb7db5dcd7
Stephen Eisenhauer <bhs2007@gmail.com>
2019-07-16 15:24:09 -0700

    Initial commit

---
+++ precache-manifest.b5b4928469782766dacc5764238c682f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bb34cfe716a6dc87e6b0d9dc1dd513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c233669ed7f8000735a",
+    "url": "/static/css/main.c8469d11.chunk.css"
+  },
+  {
+    "revision": "d7795a730563f6b0a85e",
+    "url": "/static/js/2.137676c1.chunk.js"
+  },
+  {
+    "revision": "8c233669ed7f8000735a",
+    "url": "/static/js/main.b23aa082.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a7f2dba6b6a087f7915b3165f3b8b4b",
+    "url": "/static/media/icon.8a7f2dba.svg"
+  }
+]);
